dc61c7739c6da29a6706deeea2de9e9bf955ef1d
tithupatel <159004330+tithupatel@users.noreply.github.com>
2024-02-05 13:25:48 -0800

    1

---
+++ Assigement/module2/m2t1.py
@@ -0,0 +1,8 @@
+num_integer = int(input("Enter The number :"))
+
+if num_integer > 0 :
+    print("Number is positeive.")
+elif num_integer < 0 :
+    print("Numberr is Nagetive .")
+else :
+    print("Number is zero")
