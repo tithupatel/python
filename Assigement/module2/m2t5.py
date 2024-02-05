40b147d8b403a325f7d207063a3ee2b9aa676f32
tithupatel <159004330+tithupatel@users.noreply.github.com>
2024-02-05 15:48:40 -0800

    1

---
+++ Assigement/module2/m2t5.py
@@ -0,0 +1,12 @@
+vowel ="aeiouAEIOU"
+
+a = input("Enter a latter : ")
+
+if a in vowel :
+    print(a,"-Given latter is vowel.")  
+    
+elif a.isalpha :
+    print("Please enter Valid latter.")
+    
+else:
+    print("Given latter is not vowel")
