e8df606b31b2110f46419270032cbe7dac1aa0e8
tithupatel <159004330+tithupatel@users.noreply.github.com>
2024-02-05 16:00:33 -0800

    1

---
+++ Assigement/module2/m2t6.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first integer: "))
+b = int(input("Enter the second integer: "))
+c = int(input("Enter the third integer: "))
+
+if a == b or b == c or a == c :
+    print("Sum of Three integer is Zero.")
+    
+else :
+    print("Sum of three integer is :",a+b+c)
---
+++ Assigement/module2/m2t7.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first integer: "))
+b = int(input("Enter the second integer: "))
+
+if a == b or a+b == 5 or a-b == 5 :
+    print("the Given value is True.",a , b)
+    
+else :
+    print("Please enter valid value!!")
