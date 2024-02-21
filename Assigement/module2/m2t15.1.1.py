7b70e90229cbd52a4ad0cf54eabe531b1924024d
tithupatel <159004330+tithupatel@users.noreply.github.com>
2024-02-21 13:18:15 -0800

    1

---
+++ Assigement/module2/m2t15.1.1.py
@@ -0,0 +1,8 @@
+# Write a Python program to count occurrences of a substring in a string
+
+s1 = input("Enter The Main String: ")
+s2 = input("Enter The Substring to Count: ")
+
+res=s1.count(s2)
+
+print(f"The Substring '{s2}' Appears {res} Times in The Main String")
