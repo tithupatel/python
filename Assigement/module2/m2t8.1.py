5aed72baee54e7bf66a2d8261dd0ca5d0d5451a8
tithupatel <159004330+tithupatel@users.noreply.github.com>
2024-02-20 17:59:16 -0800

    1

---
+++ Assigement/module2/m2t8.1.py
@@ -0,0 +1,15 @@
+#Write a Python program to test whether a passed letter is a vowel or not.
+letter=input("Enter a alphabet : ")
+
+if (letter =='a'):
+    print("letter is vowel")
+elif(letter =='e'):
+    print("letter is vowel")
+elif(letter =='i'):
+    print("letter is vowel")
+elif(letter =='o'):
+    print("letter is vowel") 
+elif(letter =='u'):
+    print("letter is vowel")              
+else:
+    print("letter is constant")    
