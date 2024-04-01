3c90ffb7e4f4fa52a54c756dabdd110626410689
tithupatel <159004330+tithupatel@users.noreply.github.com>
2024-04-01 12:03:23 -0700

    1

---
+++ Assigement/module4/m4t8.py
@@ -0,0 +1,18 @@
+# Write a Python program to read a file line by line and store it into a list
+
+def line_store_list(file_path) :
+    
+    try :
+        file = open(file_path,'r')
+        line = file.readlines()
+        print("Contents of the file stored in a list : ",line)
+        
+    except FileNotFoundError :
+        print("File not found")
+        
+    except Exception as e :
+        print("An error occurred:", e)
+        
+file_path = "C:\\Users\\Lenovoṇ\\OneDrive\\Desktop\\python\\python23\\Assigement\\module4(Advance python programming)\\file.txt"
+line = line_store_list(file_path)
+        
