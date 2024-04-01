dc26cd0ba385e8af88f06fb941bc8a1de2a21b96
tithupatel <159004330+tithupatel@users.noreply.github.com>
2024-04-01 11:23:10 -0700

    1

---
+++ Assigement/module4/m4t2.py
@@ -0,0 +1,18 @@
+# Write a Python program to read an entire text file
+
+def read_text_file(file_path) :
+    
+    try :
+        file = open(file_path,"r")
+        print(file.readline())
+        file.close
+        
+    except FileNotFoundError :
+        print("File not Found !!")
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        
+
+file_path = "C:\\Users\\Lenovoṇ\\OneDrive\\Desktop\\python\\python23\\Assigement\\module4(Advance python programming)\\file.txt"
+red = read_text_file(file_path)
